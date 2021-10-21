9217acb720f74208026559b0f16b1189980a058f
Robin Stenskytt <robin.olivemark@gmail.com>
2021-10-21 22:11:19 +0200

    Doing wacky shit in GUITesting

--- src/Exercises/GUITesting.java
+++ src/Exercises/GUITesting.java
@@ -1,4 +1,19 @@
 package Exercises;
 
+import javax.swing.*;
+
 public class GUITesting {
+
+    GUITesting(){
+        //alternativt kan man låta denna klass extenda JFrameDemo
+        //Då behövs inget JFrame-objekt och istf. "jf." används "this."
+        JFrame jf = new JFrame();
+        jf.setTitle("En titel");
+        jf.setSize(500, 200);
+        //jf.setLocation(100, 50);
+        jf.setLocationRelativeTo(null);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
+    }
+
 }
